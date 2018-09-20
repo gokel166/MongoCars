4abb8b38aec12302caf59b998b2048c524b3818e
gokel166 <okellogeorge40@yahoo.com>
2018-09-19 22:19:22 -0500

    Adding dependencies

--- server/models/Users.js
+++ server/models/Users.js
@@ -10,6 +10,10 @@ const UserSchema = new mongoose.Schema({
         type: String,
         default: ''
     },
+    email: {
+        type: String,
+        required: true
+    },
     isDeleted: {
         type: Boolean,
         default: false
